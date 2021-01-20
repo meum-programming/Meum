9fd39a8c03d15a0272928e9a50d610ca0a2b488b
Cheolwan Park <cheolwan.park552@gmail.com>
2021-01-20 15:28:12 +0900

    Changed setting menu size

---
+++ Assets/Scripts/UI/DisablePlayerInputOnEnabled.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace UI
+{
+    public class DisablePlayerInputOnEnabled : MonoBehaviour
+    {
+        [SerializeField] private InputActionAsset playerInput;
+
+        private void OnEnable()
+        {
+            playerInput.Disable();
+        }
+
+        private void OnDisable()
+        {
+            playerInput.Enable();
+        }
+    }
+}
