083b15b88066d6d13c44bf3546215bbcbde0d7b7
meum-programming <2master.meum@gmail.com>
2021-03-09 15:54:07 +0900

    유투브 플레이어 커밋

---
+++ Assets/Scripts/Test(Dummy)/MP4Test_youtube.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MP4Test_youtube : MonoBehaviour
+{
+    public bool autoPlay = false;
+    public string url;
+    [SerializeField] private YoutubePlayer player;
+    [SerializeField] private Text loadingText;
+    [SerializeField] private InputField inputField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.autoPlayOnStart = false;
+        player.videoQuality = YoutubePlayer.YoutubeVideoQuality.STANDARD;
+
+        if (autoPlay)
+        {
+            PlayBtnClick();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void PlayBtnClick()
+    {
+        StartCoroutine(LoadingVideo());
+    }
+
+    IEnumerator LoadingVideo()
+    {
+        int waitTime = 0;
+        
+        int.TryParse(inputField.text,out waitTime);
+
+        if (waitTime > 0)
+        {
+            for (int i = waitTime; i >= 0; i--)
+            {
+                loadingText.text = string.Format("Loading .. " + i);
+                yield return new WaitForSeconds(1);
+            }
+        }
+
+        loadingText.text = string.Empty;
+
+        player.Play(url);
+        player.videoPlayer.GetTargetAudioSource(0).mute = true;
+        player.videoPlayer.SetDirectAudioMute(0,true);
+    }
+
+}
--- Assets/Scripts/Test(Dummy)/TestMp4.cs
+++ Assets/Scripts/Test(Dummy)/TestMp4.cs
@@ -1,18 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class TestMp4 : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
 
+    public InputField inputField;
+    public Text text;
+
+    public bool autoPlay = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "1024p-1 1.mp4");
+        text.text = Application.streamingAssetsPath;
+
+        if (autoPlay)
+        {
+            VideoPlay();
+
+            //videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "1024p-1.mp4");
+            //videoPlayer.url = "https://meumbucket.s3.ap-northeast-2.amazonaws.com/MP4Test/Test.mp4";
+            //videoPlayer.url = "https://www.youtube.com/watch?v=iQZobAhgayA";
+            //videoPlayer.Play();
+        }
+        
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        Debug.LogWarning("Play ON!");
+
+        UnityWebRequest www = new UnityWebRequest(uri);
+        www.downloadHandler = new DownloadHandlerBuffer();
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+
+            //www.downloadHandler.ve
+
+            Debug.LogWarning("results = " + results.Length);
+
+        }
+
+    }
+
+    public void VideoPlay()
+    {
+        string url = inputField.text;
+
+        Debug.LogWarning("url = " + url);
+
+        videoPlayer.url = url;
         videoPlayer.Play();
+
+        //StartCoroutine(GetRequest(url));
+
     }
 
     // Update is called once per frame
